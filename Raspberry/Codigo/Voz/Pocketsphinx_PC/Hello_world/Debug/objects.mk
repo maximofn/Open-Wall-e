235afa175821a8683184b210acb18d9dd07649d2
MaximoFN <maximofn@gmail.com>
2016-10-20 23:11:14 +0200

    Se elimina el royecto Hello_world hecho con eclipse

--- Raspberry/Codigo/Voz/Pocketsphinx_PC/Hello_world/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpocketsphinx -lsphinxbase -lsphinxad -lpulse -lpulse-simple -lpthread -lm
-
