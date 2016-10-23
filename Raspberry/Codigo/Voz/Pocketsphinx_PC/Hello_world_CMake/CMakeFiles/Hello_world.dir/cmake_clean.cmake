e58d608ad03f656b4c43b5f5b56d19e71320fdd4
MaximoFN <maximofn@gmail.com>
2016-10-23 20:13:26 +0200

    No hay mucho cambio

---
+++ Raspberry/Codigo/Voz/Pocketsphinx_PC/Hello_world_CMake/CMakeFiles/Hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_world.dir/Hello_world.cpp.o"
+  "Hello_world.pdb"
+  "Hello_world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
